739e70dfc1a6ba53fba2844bb33d77522d1b9b78
Joshua <109425132+Zackking1542@users.noreply.github.com>
2023-01-06 17:18:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload() {
+}
+
+function setup() {
+    canvas = creativeCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw() {
+    Image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    SVGAElement('studen_name.png');
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
